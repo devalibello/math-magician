3703f6d8b7a371301637cdb156e14d5791cc5332
devalibello <alibelloimoukhuede@gmail.com>
2023-06-15 16:42:54 +0300

    Add SPA using react

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import style from '../styles/Navbar.module.css';
+
+const Navbar = () => (
+  <nav className="nav-page">
+    <ul className={style.navContainer}>
+      <li className={style.mathMagician}>Math Magicians</li>
+      <li><Link to="/">Home</Link></li>
+      <li>&#124;</li>
+      <li><Link to="/calculator">Calculator</Link></li>
+      <li>&#124;</li>
+      <li><Link to="/quote">Quote</Link></li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
